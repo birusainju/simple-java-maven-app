dfb4eafb93df65d34281c4cdea300676a9e2e16f
birusainju <62688359+birusainju@users.noreply.github.com>
2022-05-31 20:36:10 +0545

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,9 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+                System.out.println("Hello warning");
+        String asdf = null;
+        System.out.println(asdf.toString());
     }
 
     private final String getMessage() {
